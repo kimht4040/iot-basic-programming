8499630aae77ea32911aceef6a17fce2c96430c7
김학태 <kimht4040@naver.com>
2026-02-10 17:11:05 +0900

    7일차

---
+++ Day07/Chap17-Solution/Chap17-01-app/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+struct student {
+	int num;
+	double grade;
+} st1;
+
+
+int main() {
+
+	st1.num = 2;
+	st1.grade = 3.5;
+	printf("학번 %d 성적 %.1f", st1.num, st1.grade);
+
+
+	return 0;
+}
